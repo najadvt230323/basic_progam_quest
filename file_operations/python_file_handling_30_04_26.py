7c4150e349317162270993f130068d290065f56e
najadvt230323 <najadvt230323@gmail.com>
2026-04-30 11:42:28 +0530

    update30_04_26

---
+++ file_operations/python_file_handling_30_04_26.py
@@ -0,0 +1,6 @@
+'''---------------python file handling_30_04_26---------------'''
+# ---------------------------------------------------------------
+
+# -->named collection of related Date
+# --> stored as a single unit on a storage
+# -->os treats it as onr objct
