becb870a3f67c745117f499a848b4af3bde9a0cc
najadvt230323 <najadvt230323@gmail.com>
2026-05-15 13:34:26 +0530

    UPDATE

--- OOPS(objact_oriented_programming)/oops.py
+++ OOPS(objact_oriented_programming)/oops.py
@@ -56,6 +56,92 @@ del Student1.school
 print(richu.school)                # kunnamagalam hss
 # print(najad.school)                # AttributeError: 'Student1' object has no attribute 'school'
 
+print()
+Student1.school = "quest"
+print(Student1.school)
+print(richu.school)
+print(najad.school)
+
+# ---------------------------------------------------------------
+
+print()
+class Student2 :
+    school="quest"
+    def __init__(self):
+        print("hi self")
+
+richu=Student2()
+najad=Student2()
+
+# -------------------------------------------------------------------
+
+print()
+class Student3 :
+    school="quest"
+    def __init__(self,roll_no,name,age):
+        print("hi Student3 class")
+        self.a=roll_no
+        self.b=name
+        self.c=age
+    def details(self):
+        print()
+        print(self.a)
+        print(self.b)
+        print(self.c)
+        
+
+# richu=Student3()                  #ypeError: Student3.__init__() missing 3 required positional arguments: 'roll_no', 'name', and 'age'
+najad=Student3(5,"najad",25)
+richu=Student3(name="richu",age=25,roll_no=10)
+
+print()
+print(najad.a,"  ",najad.b,"  ",najad.c)
+print(richu.a,"  ",richu.b,"  ",richu.c)
+print()
+najad.details()
+richu.details()
+
+# -------------------------------------------------------
+
+print()
+class Employee:
+    company="quest"
+    branch="calicut"
+    def __init__(self,a,b,c,d):
+        self.emp_id=a
+        self.emp_name=b
+        self.emp_salary=c
+        self.emp_email=d
+    def details(self):
+        print(f"emp_id : {self.emp_id}\nemp_name : {self.emp_name}\nemp_salary : {self.emp_salary}\nemp_email : {self.emp_email}")
+        print()
+    def update(self):
+        if self.emp_salary>10000 :
+            self.emp_salary=self.emp_salary+self.emp_salary*0.15
+    def resin(self):
+        self.company = "filpkart"
+        self.branch = "kochi"
+        self.emp_id = 101
+        self.emp_salary=150000
+
+
+richu=Employee(10,"richu",15000,"richu.com")
+print(richu.company,"   ",richu.branch)
+richu.details()
+najad=Employee(15,"najad",350000,"najad.com")
+najad.details()
+
+najad.update()
+najad.details()
+
+najad.resin()
+print(najad.company)
+print(najad.branch)
+najad.details()
+
+# ----------------------------------------------------------------------------
+      
+
 
 
 
