8f148f452c8e64e7cd247f9eb6b651c6c51201ca
najadvt230323 <najadvt230323@gmail.com>
2026-05-26 12:05:58 +0530

    update

--- Assignment-FileOperation20-05-26/Assig-FileOperation20-05-26.py
+++ Assignment-FileOperation20-05-26/Assig-FileOperation20-05-26.py
@@ -180,11 +180,16 @@ email : {v[1]}
             f=False
             while h:
                 name1=input("Enter name: ")
-                if name1.isalpha() and len(name1)<60 and len(name1)>=4:
-                    h=False
-                    f=True
+                for k,v in a1.items():
+                    o=0
+                    if k==name1:
+                        h=False
+                        f=True
+                        o=1
+                if o==1:
+                    pass           
                 else:
-                    print("Enter valide name (\"not use spce\" , \"not use num\").")
+                    print("Name not in record,Enter valide name.")
 
             while f:
                 b=input("Enter Phone number (+91XXXXXXXXXX) : ")
@@ -354,17 +359,143 @@ email : {v[1]}
                                                                             print("phon number start with \"+91\"")
                                                                     else:
                                                                         print("Enter validen phon number.\n")
-                                                                    
-
-
+                                                        else:
+                                                            print("Enter validen opstion.")                                                                 
+                                            elif m=="2":
+                                            #2. email edit
+                                            #  ======================
+                                                q=True
+                                                if len(v[1])==1:
+                                                    while q :
+                                                        r=input("1. Add email id 2. Edit email id \nEnter your option : ")
+                                                        if r=="1":
+                                                            u=True
+                                                            while u :
+                                                                s=input("Enter email id : ")
+                                                                if s.endswith("@gmail.com") and len(s)>12 and s.isascii() and len(s)<50:
+                                                                    l=0
+                                                                    for w,x in a1.items():
+                                                                        for i in range(len(x[1])):
+                                                                            if s==x[1][i]:
+                                                                                l=1
+                                                                    if l==1:
+                                                                        print("Email id aready existed.")
+                                                                    else:                                   
+                                                                        v[1].append(s)
+                                                                        with open("assig.txt","w") as new :
+                                                                            for k,v in a1.items():
+                                                                                t="{"+f"\'{k}\': {v}"+"}"+"\n"
+                                                                                new.write(t)
+                                                                        print("Adding email id successfully 👍")
+                                                                        p=False
+                                                                        q=False
+                                                                        u=False
+                                                                
+                                                                else:
+                                                                    print("Enter validen email id.\n")
+                                                        elif r=="2":
+                                                            u=True
+                                                            while u :
+                                                                s=input("Editig email id \nEnter new email id :")
+                                                                if s.endswith("@gmail.com") and len(s)>12 and s.isascii() and len(s)<50:
+                                                                    l=0
+                                                                    for w,x in a1.items():
+                                                                        for i in range(len(x[1])):
+                                                                            if s==x[1][i]:
+                                                                                l=1
+                                                                    if l==1:
+                                                                        print("Email id aready existed.")
+                                                                    else:                                   
+                                                                        v[1][0]=s
+                                                                        with open("assig.txt","w") as new :
+                                                                            for k,v in a1.items():
+                                                                                t="{"+f"\'{k}\': {v}"+"}"+"\n"
+                                                                                new.write(t)
+                                                                        print("Editing phon number successfully 👍")
+                                                                        p=False
+                                                                        q=False
+                                                                        u=False
+                                                                else:
+                                                                    print("Enter validen email id.\n")
                                                         else:
                                                             print("Enter validen opstion.")
-                                                        print(f"1 . {v[0][0]} 2 . {v[0][1]}")
-                                                                 
-                                            elif m=="2":
-                                            #1. email edit
-                                            #  ======================
-                                                pass
+                                                elif len(v[0])==2:
+                                                    q=True
+                                                    while q:
+                                                        r=input("1. Deleat email id 2.Edit email id \nEnter your option : ")
+                                                        if r=='1':
+                                                            s=input(f"1 . {v[1][0]} 2 . {v[1][1]}\nWhich email should be deleted? :  ")
+                                                            if s=='1':
+                                                                del v[1][0]
+                                                                with open("assig.txt","w") as new :
+                                                                    for k,v in a1.items():
+                                                                        t="{"+f"\'{k}\': {v}"+"}"+"\n"
+                                                                        new.write(t)
+                                                                print("delete email id successfully 👍")
+                                                                p=False
+                                                                q=False
+                                                            elif s=='2':
+                                                                del v[1][1]
+                                                                with open("assig.txt","w") as new :
+                                                                    for k,v in a1.items():
+                                                                        t="{"+f"\'{k}\': {v}"+"}"+"\n"
+                                                                        new.write(t)
+                                                                print("delete email successfully 👍")
+                                                                p=False
+                                                                q=False
+                                                            else:
+                                                                print("Enter validen opstion.")
+                                                        elif r=='2':
+                                                            u=True
+                                                            while u:
+                                                                s=input(f"1 . {v[1][0]} 2 . {v[1][1]}\nWhich number should be Edit? :  ")
+                                                                if s=='1':
+                                                                    s=input("Enter new email id : ")
+                                                                    if s.endswith("@gmail.com") and len(s)>12 and s.isascii() and len(s)<50:
+                                                                        l=0
+                                                                        for w,x in a1.items():
+                                                                            for i in range(len(x[1])):
+                                                                                if s==x[1][i]:
+                                                                                    l=1
+                                                                        if l==1:
+                                                                            print("email id aready existed.")
+                                                                        else: 
+                                                                            v[1][0]=s
+                                                                            with open("assig.txt","w") as new :
+                                                                                for k,v in a1.items():
+                                                                                    t="{"+f"\'{k}\': {v}"+"}"+"\n"
+                                                                                    new.write(t)
+                                                                            p=False
+                                                                            q=False
+                                                                            u=False
+                                                                            print("Edit email id successfully 👍")             
+                                                                    else:
+                                                                        print("Enter validen email.\n")
+                                                                    
+                                                                elif s=='2':
+                                                                    s=input("Enter new email id : ")
+                                                                    if s.endswith("@gmail.com") and len(s)>12 and s.isascii() and len(s)<50:
+                                                                        l=0
+                                                                        for w,x in a1.items():
+                                                                            for i in range(len(x[1])):
+                                                                                if s==x[1][i]:
+                                                                                    l=1
+                                                                        if l==1:
+                                                                            print("email id aready existed.")
+                                                                        else: 
+                                                                            v[1][1]=s
+                                                                            with open("assig.txt","w") as new :
+                                                                                for k,v in a1.items():
+                                                                                    t="{"+f"\'{k}\': {v}"+"}"+"\n"
+                                                                                    new.write(t)
+                                                                            p=False
+                                                                            q=False
+                                                                            u=False
+                                                                            print("Edit email id successfully 👍")             
+                                                                    else:
+                                                                        print("Enter validen email.\n")
+                                                        else:
+                                                            print("Enter validen opstion.")
                                             else :
                                                 print("Enter validen opstion.")
 
@@ -384,8 +515,60 @@ email : {v[1]}
 
 
         elif op == 4 :
-            pass
+            print("<---------------4. Delete Record-------------->")
+            h=True
+            f=False
+            while h:
+                name1=input("Enter name: ")
+                for k,v in a1.items():
+                    o=0
+                    if k==name1:
+                        h=False
+                        f=True
+                        o=1
+                if o==1:
+                    pass           
+                else:
+                    print("Name not in record,Enter valide name.")
 
+            while f:
+                b=input("Enter Phone number (+91XXXXXXXXXX) : ")
+                if len(b)==13 or len(b)==10 and b[1:].isdecimal() :
+                    if b.startswith("+91") and len(b)==13 :
+                        f=False
+                        for k,v in a1.items():
+                            o=0
+                            if k==name1:
+                                o=1
+                                for i in range(len(v[0])):
+                                    n=0
+                                    if v[0][i]==b:
+                                        n=1
+                                        print(f'''
+name : {k}  
+phon no : {v[0]}  
+email : {v[1]}
+''')
+                                        print("Delete Record?")
+                                        p=True
+                                        while p :
+                                            m=input("1. yes 2. No \nEnter your option : ")
+                                            if m=='1':
+                                                print("delete record successfully 👍")
+                                                f=False
+                                                p=False
+                                            elif m=="2":
+                                                print("Record as not delete.")
+                                                f=False
+                                                p=False
+                                            else:
+                                                print("Enter validen opstion")
+                        if m=='1':
+                            del a1[name1]
+                            with open("assig.txt","w") as new :
+                                for k,v in a1.items():
+                                    t="{"+f"\'{k}\': {v}"+"}"+"\n"  
+                                    new.write(t)                                           
         elif op == 5 :
             print("Exiting... Thank You 😊")
             a=False
@@ -395,13 +578,3 @@ email : {v[1]}
     
     else:
         print("Enter validen opstion")
-
-
-
-
-
-
-
-
-
-
