1c6c5cc49d8241b558af5977e152916d77889692
najadvt230323 <najadvt230323@gmail.com>
2026-06-03 12:18:43 +0530

    update

--- REGULAR EXPRESSION/regular expression.py
+++ REGULAR EXPRESSION/regular expression.py
@@ -143,8 +143,48 @@ print((re.search(r"phon",data1)).start())                  #output : 3
 print((re.search(r"phon",data1)).end())                    #output : 7
 print((re.search(r"phon",data1)).span())                   #output : (3, 7)
 
+print(re.findall(r"phon",data1))                           #output : ['phon']
+print(re.findall(r"\d",data1))                             #output : ['9', '5', '6', '2', '0', '2', '0', '2', '0', '2', '0', '7']
+print(re.findall(r"\d\d",data1))                           #output : ['95', '62', '02', '02', '02', '07']
+print(re.findall(r"\d{5}",data1))                          #output : ['95620', '20202']
+print(re.findall(r"\d{7}",data1))                          #output : ['9562020']
+print(re.findall(r"\d{7,}",data1))                         #output : ['956202020207']
+print(re.findall(r"\d{3,5}",data1))                        #output : ['95620', '20202']
+print(re.findall(r"\d{,3}",data1))#output : ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '956', '202', '020', '207', '', '', '', '', '', '', '', '', '', '', '']
 
+print(re.findall(r"\w{3}",data1))                           #output : ho', 'num', 'ber', '956', '202', '020', '207']
+print(re.findall(r"\w{3,}",data1))                          #output : ['phone', 'number', '956202020207']
+print(re.findall(r"\w{3,5}",data1))                         #output : ['phone', 'numbe', '95620', '20202']
+print(re.findall(r"\w{,6}",data1))#output : ['my', '', 'phone', '', 'number', '', 'is', '', '956202', '020207', '', '', '', '', '', '', '', '', '', '', '']
 
+# --------------------------------------------------------------------------------------------------------
+print()
+data1="my phone -2   number 3542 is 956202020207 \n *^@ 😍👌😁"
 
+print(re.findall(r"\d+",data1))                             #output : ['2', '3542', '956202020207']
+print(re.findall(r"\d+4",data1))                             #output : ['354']
+print(re.findall(r"\d+0",data1))                             #output : ['95620202020']
 
+print(re.findall(r"\w+",data1))                             #output : ['my', 'phone', '2', 'number', '3542', 'is', '956202020207']
+print(re.findall(r"\w+4",data1))                             #output : ['354']
+print(re.findall(r"\w+e",data1))                             #output : ['phone', 'numbe']
 
+print()
+print(re.findall(r"\d*",data1))#output : ['', '', '', '', '', '', '', '', '', '', '2', '', '', '', '', '', '', '', '', '', '', '3542', '', '', '', '', '956202020207', '', '', '', '', '', '', '', '', '', '', '']
+print(re.findall(r"\d*4",data1))                             #output : ['354']
+print(re.findall(r"\d*0",data1))                             #output : ['95620202020']
+
+print(re.findall(r"\w*",data1))#output : ['my', '', 'phone', '', '', '2', '', '', '', 'number', '', '3542', '', 'is', '', '956202020207', '', '', '', '', '', '', '', '', '', '', '']
+print(re.findall(r"\w*4",data1))                             #output : ['354']
+print(re.findall(r"\w*e",data1))                             #output : ['phone', 'numbe']
+
+print()
+print(re.findall(r"\d?",data1))#output : ['', '', '', '', '', '', '', '', '', '', '2', '', '', '', '', '', '', '', '', '', '', '3', '5', '4', '2', '', '', '', '', '9', '5', '6', '2', '0', '2', '0', '2', '0', '2', '0', '7', '', '', '', '', '', '', '', '', '', '', '']
+print(re.findall(r"\d?4",data1))                             #output : ['54']
+print(re.findall(r"\d?0",data1))                             #output : ['20', '20', '20', '20']
+
+print(re.findall(r"\w?",data1))#output : ['m', 'y', '', 'p', 'h', 'o', 'n', 'e', '', '', '2', '', '', '', 'n', 'u', 'm', 'b', 'e', 'r', '', '3', '5', '4', '2', '', 'i', 's', '', '9', '5', '6', '2', '0', '2', '0', '2', '0', '2', '0', '7', '', '', '', '', '', '', '', '', '', '', '']
+print(re.findall(r"\w?4",data1))                             #output : ['54']
+print(re.findall(r"\w?e",data1))                             #output : ['ne', 'be']
+
+# -----------------------------------------------------------------------------------------------------
