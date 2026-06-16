fb0b8b996a2142bc2c71757eeab1f3c5a2b2c71b
najadvt230323 <najadvt230323@gmail.com>
2026-06-16 13:05:15 +0530

    update

--- email/test.py
+++ email/test.py
@@ -1,18 +1,40 @@
+# import smtplib
+
+# s = smtplib.SMTP("smtp.gmail.com",587)
+
+# s.starttls()
+
+# s.login("najadvt230323@gmail.com","shbz lfty qywu pbao")
+
+# msg = "this is a testing mail"
+
+# s.sendmail("najadvt230323@gmail.com","sreerajmathiyath6785@gmail.com",msg)
+
+# s.quit
+
+# ----------------------------------------------------------------------------------------
+
 import smtplib
+from email.mime.text import MIMEText
 
-s = smtplib.SMTP("smtp.gmail.com",587)
+sender_email = "najadvt230323@gmail.com"
+app_password = "shbz lfty qywu pbao"
+recever_email = "sreerajmathiyath6785@gmail.com"
 
-s.starttls()
+html = " <h2 style='color: blue;background-color: pink;'> Hello Welcome Najad...</h2><br> <p style='color: green; background-color: lightgray;'> You are hired......</p>"
 
-s.login("najadvt230323@gmail.com","shbz lfty qywu pbao")
+# message = MIMEText(html,"plain")
+message = MIMEText(html,"html")
+message["subject"] = "greeting"
+message["from"] = sender_email
+message["to"] = recever_email
 
-msg = "this is a testing mail"
+with smtplib.SMTP("smtp.gmail.com",587) as s :
+    s.starttls()
+    s.login(sender_email,app_password)
+    s.send_message(message)
 
-s.sendmail("najadvt230323@gmail.com","sreerajmathiyath6785@gmail.com",msg)
-
-s.quit
-
-
+print("email sent successfully")
 
 
 
