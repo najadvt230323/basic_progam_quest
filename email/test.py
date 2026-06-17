75c1829c319314c1c8be024d3414519c7e5ac1cb
najadvt230323 <najadvt230323@gmail.com>
2026-06-17 11:59:31 +0530

    UPDATE

--- email/test.py
+++ email/test.py
@@ -1,18 +1,25 @@
-# import smtplib
+'''
 
-# s = smtplib.SMTP("smtp.gmail.com",587)
+import smtplib
 
-# s.starttls()
+s = smtplib.SMTP("smtp.gmail.com",587)
 
-# s.login("najadvt230323@gmail.com","shbz lfty qywu pbao")
+s.starttls()
 
-# msg = "this is a testing mail"
+s.login("najadvt230323@gmail.com","shbz lfty qywu pbao")
 
-# s.sendmail("najadvt230323@gmail.com","sreerajmathiyath6785@gmail.com",msg)
+msg = "this is a testing mail"
 
-# s.quit
+s.sendmail("najadvt230323@gmail.com","sreerajmathiyath6785@gmail.com",msg)
+
+s.quit
+
+'''
 
 # ----------------------------------------------------------------------------------------
+
+'''
+# HTML CONTENT
 
 import smtplib
 from email.mime.text import MIMEText
@@ -25,9 +32,9 @@ html = " <h2 style='color: blue;background-color: pink;'> Hello Welcome Najad...
 
 # message = MIMEText(html,"plain")
 message = MIMEText(html,"html")
-message["subject"] = "greeting"
-message["from"] = sender_email
-message["to"] = recever_email
+message["Subject"] = "greeting"
+message["From"] = sender_email
+message["To"] = recever_email
 
 with smtplib.SMTP("smtp.gmail.com",587) as s :
     s.starttls()
@@ -36,11 +43,58 @@ with smtplib.SMTP("smtp.gmail.com",587) as s :
 
 print("email sent successfully")
 
+'''
+
+# --------------------------------------------------------------------------------------------
+
+# ''' 
+
+# Test email with attachment
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+sender_email = "najadvt230323@gmail.com"
+app_password = "shbz lfty qywu pbao"
+recever_email = "sreerajmathiyath6785@gmail.com"
 
 
+message = MIMEMultipart()
+message["Subject"] = "Test email with attachment"
+message["From"] = sender_email
+message["To"] = recever_email
+
+body = "hello,\nthis email with attachment."
+message.attach(MIMEText(body,"plain"))
+
+file_path = r"C:\Users\NAJAD V T\Desktop\python full stak\python\loki-unexpected-moment-rh.jpg"
+file_name = "loki"
+
+try:
+    with open(file_path,"rb") as s:
+
+        mime = MIMEBase("application","octet-stream")
+        mime.set_payload(s.read())
+        encoders.encode_base64(mime)
+        mime.add_header("Content-Disposition",f"attachment; filename={file_name}")
+        message.attach(mime)
+except Exception as e:
+    print(f"an exception occures {e}")
 
 
+try:
+    with smtplib.SMTP("smtp.gmail.com",587) as s :
+        s.starttls()
+        s.login(sender_email,app_password)
+        s.send_message(message)
 
+    print("email sent successfully")  
+except Exception as e:
+    print(f"an exception occures {e}")
 
+#'''
 
 
