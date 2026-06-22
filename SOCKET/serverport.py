ecc341b25642a944a2a24b991ba2327a5a990bed
najadvt230323 <najadvt230323@gmail.com>
2026-06-22 12:32:04 +0530

    update

---
+++ SOCKET/serverport.py
@@ -0,0 +1,34 @@
+import socket
+
+server = socket.socket()
+
+host = "127.0.0.1"
+port = 5001
+
+server.bind((host,port))
+
+server.listen()
+
+conn , addr = server.accept()
+
+print(conn)
+print(addr)
+
+print("connection from : ",str (addr))
+
+while True:
+
+    data = conn.recv(1024).decode()
+
+    if not data :
+        break
+    
+    data = str(data).upper()
+
+    print(f"from cilent : {data}")
+
+    data = input("type message : ")
+
+    conn.send(data.encode())
+
+conn.close()
