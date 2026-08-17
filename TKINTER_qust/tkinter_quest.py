470696182cdb730563b956feeaa286793a38df8d
najadvt230323 <najadvt230323@gmail.com>
2026-08-17 13:46:58 +0530

    update

--- TKINTER_qust/tkinter_quest.py
+++ TKINTER_qust/tkinter_quest.py
@@ -1,6 +1,102 @@
+# from tkinter import *
+# a=Tk()
+# a.mainloop()
+
+# ===================================
+# from tkinter import *
+# a=Tk()
+# a.title("my first program")
+# a.geometry("300x400")
+# a.minsize(200,300)
+# a.maxsize(400,500)
+
+# b=Label(text="hai")
+# b.pack()
+# b=Label(text="hai najad")
+# b.pack()
+# b=Label(text="hai quest" \
+# " hi aju")
+# b.pack()
+# b=Label(text='''hai quest
+#  hi aju ''')
+# b.pack()
+
+# c=Label(text="najad")
+# b.grid()
+
+# a.mainloop()
+# =================================================
+
+# from tkinter import *
+# a=Tk()
+# a.title("my first program")
+# a.geometry("300x400")
+# a.minsize(200,300)
+# a.maxsize(400,500)
+
+# c=Label(text="najad")
+# c.grid(row=0,column=0)
+
+# c=Label(text="hi")
+# c.grid(row=1,column=1)
+
+# c=Label(text="najad")
+# c.grid(row=2,column=2)
+
+# c=Label(text="hi")
+# c.grid(row=3,column=3)
+
+# c=Label(text="najad")
+# c.grid(row=4,column=4)
+
+# c=Label(text="najad")
+# c.grid(row=18,column=18)
+
+# c=Label(text="hi")
+# c.grid(row=8,column=8)
+
+# c=Label(text="najad hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+# c.grid(row=4,column=0)
+
+# a.mainloop()
+# ================================================================================
+
 from tkinter import *
 a=Tk()
+a.title("my first program")
+a.geometry("300x400")
+a.minsize(200,300)
+a.maxsize(400,500)
+
+a=Label(text="enter your name : ")
+a.grid(row=0,column=0)
+box1=Entry()
+box1.grid(row=0,column=1)
+
+a=Label(text="enter your age : ")
+a.grid(row=1,column=0)
+box1=Entry()
+box1.grid(row=1,column=1)
+
+a=Label(text="enter your place : ")
+a.grid(row=2,column=0)
+box1=Entry()
+box1.grid(row=2,column=1)
+
+
+# d=
 a.mainloop()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
