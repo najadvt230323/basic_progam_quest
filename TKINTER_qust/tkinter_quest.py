046e0ec79eed615e0851e48ca746935dd743a001
najadvt230323 <najadvt230323@gmail.com>
2026-08-19 14:47:10 +0530

    update

--- TKINTER_qust/tkinter_quest.py
+++ TKINTER_qust/tkinter_quest.py
@@ -61,34 +61,259 @@
 # a.mainloop()
 # ================================================================================
 
+# def onclick():
+#     v=box1.get()
+#     m=Label()
+#     m.grid(row=4,column=1)
+#     m.config(text=v)
+
+#     m=Label()
+#     m.grid(row=5,column=1)
+#     m.config(text=v[::-1])
+
+
+
+# from tkinter import *
+# a=Tk()
+# a.title("my first program")
+# a.geometry("300x400")
+# a.minsize(200,300)
+# a.maxsize(400,500)
+
+# a=Label(text="enter your name : ")
+# a.grid(row=0,column=0)
+# box1=Entry()
+# box1.grid(row=0,column=1)
+
+# a=Label(text="enter your age : ")
+# a.grid(row=1,column=0)
+# box2=Entry()
+# box2.grid(row=1,column=1)
+
+# a=Label(text="enter your place : ")
+# a.grid(row=2,column=0)
+# box3=Entry()
+# box3.grid(row=2,column=1)
+
+# btn=Button(text="register" , command=onclick)
+# btn.grid(row=3 , column=1)
+
+# a.mainloop()
+# =====================================================================
+
+# def onclick():
+#     c=box1.get()
+#     d=box2.get()
+
+#     if c.isdigit() and d.isdigit() :
+#         m=Label()
+#         m.grid(row=4,column=1)
+#         m.config(text=(f"add={int(c)+int(d)}"))
+
+#         m=Label()
+#         m.grid(row=5,column=1)
+#         m.config(text=(f"sub={int(c)-int(d)}"))
+
+#         m=Label()
+#         m.grid(row=6,column=1)
+#         m.config(text=(f"mult={int(c)*int(d)}"))
+#     else :
+#         m=Label()
+#         m.grid(row=4,column=1)
+#         m.config(text="enter 2 intger number ")
+
+
+# from tkinter import *
+# a=Tk()
+# a.title("my first program")
+# a.geometry("300x400")
+# a.minsize(200,300)
+# a.maxsize(400,500)
+
+# a=Label(text="enter 1st intger number : ")
+# a.grid(row=0,column=0)
+# box1=Entry()
+# box1.grid(row=0,column=1)
+
+# b=Label(text="enter 2nd intger number : ")
+# b.grid(row=1,column=0)
+# box2=Entry()
+# box2.grid(row=1,column=1)
+
+# btn=Button(text="math" , command=onclick)
+# btn.grid(row=2 , column=1)
+
+# # a=
+
+# a.mainloop()
+
+# ==============================================================
+
+
+# def onclick1():
+#     c=box1.get()
+#     d=box2.get()
+
+#     if c.isdigit() and d.isdigit() :
+#         # m=Label()
+#         # m.grid(row=2,column=2)
+#         # m.config(text=int(c)+int(d))
+
+#         box=Entry()
+#         box.grid(row=2 , column=2)
+#         box.insert(0,int(c)+int(d))
+
+
+
+#     else :
+#         m=Label()
+#         m.grid(row=2,column=1)
+#         m.config(text="enter 2 intger number ")
+
+# def onclick2():
+#     c=box1.get()
+#     d=box2.get()
+
+
+#     if c.isdigit() and d.isdigit() :
+#         m=Label()
+#         m.grid(row=3,column=2)
+#         m.config(text=int(c)-int(d))
+
+#     else :
+#         m=Label()
+#         m.grid(row=2,column=1)
+#         m.config(text="enter 2 intger number ")
+
+# def onclick3():
+#     c=box1.get()
+#     d=box2.get()
+
+#     if c.isdigit() and d.isdigit() :
+#         m=Label()
+#         m.grid(row=4,column=2)
+#         m.config(text=int(c)*int(d))
+
+#     else :
+#         m=Label()
+#         m.grid(row=2,column=1)
+#         m.config(text="enter 2 intger number ")
+
+# def onclick4():
+#     c=box1.get()
+#     d=box2.get()
+
+#     if c.isdigit() and d.isdigit() :
+#         m=Label()
+#         m.grid(row=5,column=2)
+#         m.config(text=int(c)/int(d))
+
+#     else :
+#         m=Label()
+#         m.grid(row=6,column=1)
+#         m.config(text="enter 2 intger number ")
+
+# from tkinter import *
+# a=Tk()
+# a.title("my first program")
+# a.geometry("300x400")
+# a.minsize(200,300)
+# a.maxsize(400,500)
+
+# a=Label(text="enter 1st intger number : ")
+# a.grid(row=0,column=0)
+# box1=Entry()
+# box1.grid(row=0,column=1)
+
+# b=Label(text="enter 2nd intger number : ")
+# b.grid(row=1,column=0)
+# box2=Entry()
+# box2.grid(row=1,column=1)
+
+# btn=Button(text="add" , command=onclick1)
+# btn.grid(row=2 , column=1)
+
+# btn=Button(text="sub" , command=onclick2)
+# btn.grid(row=3 , column=1)
+
+# btn=Button(text="mult" , command=onclick3)
+# btn.grid(row=4 , column=1)
+
+# btn=Button(text="div" , command=onclick4)
+# btn.grid(row=5 , column=1)
+
+# # a=
+
+# a.mainloop()
+
+# ===================================================================
+def onclick1():
+    f=box1.get()
+
+    try:
+        f=float(f)
+        c=(f-32)*5/9
+
+        box=Entry()
+        box.grid(row=0 , column=3)
+        box.insert(0,c)
+    except:
+        box=Entry()
+        box.grid(row=0 , column=3)
+        box.insert(0,"enter a number ")
+        # m=Label()
+        # m.grid(row=0,column=3)
+        # m.config(text="enter a number ") 
+
+    # if f.isdecimal():
+    #     c=(int(f)-32)*5/9
+
+    #     box=Entry()
+    #     box.grid(row=0 , column=3)
+    #     box.insert(0,c)
+    # else :
+    #     m=Label()
+    #     m.grid(row=0,column=3)
+    #     m.config(text="enter intger number ")
+
+def onclick2():
+    c=box2.get()
+    try:
+        c=float(c)
+        f=(c*9/5)+32
+
+        box=Entry()
+        box.grid(row=1 , column=3)
+        box.insert(0,f)
+    except :
+        box=Entry()
+        box.grid(row=1 , column=3)
+        box.insert(0,"enter a number ")
+
+
 from tkinter import *
 a=Tk()
-a.title("my first program")
-a.geometry("300x400")
-a.minsize(200,300)
-a.maxsize(400,500)
+a.title("convert to celcius")
+a.geometry("500x500")
+a.minsize(500,500)
+a.maxsize(500,500)
 
-a=Label(text="enter your name : ")
-a.grid(row=0,column=0)
+b=Label(text="enter farenheit :")
+b.grid(row=0,column=0)
 box1=Entry()
 box1.grid(row=0,column=1)
+btn=Button(text="convert to celcius" , command=onclick1)
+btn.grid(row=0,column=2)
 
-a=Label(text="enter your age : ")
-a.grid(row=1,column=0)
-box1=Entry()
-box1.grid(row=1,column=1)
+b=Label(text="enter celcius :")
+b.grid(row=1,column=0)
+box2=Entry()
+box2.grid(row=1,column=1)
+btn=Button(text="convert to farenheit" , command=onclick2)
+btn.grid(row=1,column=2)
 
-a=Label(text="enter your place : ")
-a.grid(row=2,column=0)
-box1=Entry()
-box1.grid(row=2,column=1)
-
-
-# d=
+# z= 
 a.mainloop()
-
-
-
 
 
 
