2a37907db1613c47e6424ed22d0c840c13ab69ec
najadvt230323 <najadvt230323@gmail.com>
2026-09-01 12:48:35 +0530

    update

--- PYTHON_DJANGO_31-08-26/furst.py
+++ PYTHON_DJANGO_31-08-26/furst.py
@@ -11,3 +11,25 @@
 
 # cd myproject
 # py manage.py startapp myapp
+
+# py manage.py runserver
+
+
+'''
+
+Watching for file changes with StatReloader
+Performing system checks...
+
+System check identified no issues (0 silenced).
+
+You have 18 unapplied migration(s). Your project may not work properly until you apply the migrations for app(s): admin, auth, contenttypes, sessions.
+Run 'python manage.py migrate' to apply them.
+September 01, 2026 - 12:45:33
+Django version 6.1, using settings 'myproject.settings'
+Starting WSGI development server at http://127.0.0.1:8000/
+Quit the server with CTRL-BREAK.
+
+WARNING: This is a development server. Do not use it in a production setting. Use a production WSGI or ASGI server instead.
+For more information on production servers see: https://docs.djangoproject.com/en/6.1/howto/deployment/
+
+'''
