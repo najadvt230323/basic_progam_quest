e17c69d26155dd22b71da9d076a0bf617749c9a3
najadvt230323 <najadvt230323@gmail.com>
2026-09-22 13:32:34 +0530

    update

--- PYTHON_DJANGO_31-08-26/myproject/myapp/views.py
+++ PYTHON_DJANGO_31-08-26/myproject/myapp/views.py
@@ -273,6 +273,10 @@ def delete_session(request) :
         del request.session["password"]
         return HttpResponse ("session is deleted")
 
+# ------------------------------------------------------------------------------------------------------
+
+
+
 
 
 
